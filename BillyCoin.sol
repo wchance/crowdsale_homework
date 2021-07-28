7db000e93f373abbbf82dbba427cb1f670093d64
William Chance <william@chance.tel>
2021-07-28 12:32:50 -0400

    Renamed the token and uploaded latest code

--- BillyCoin.sol
+++ BillyCoin.sol
@@ -5,13 +5,8 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 contract PupperCoin is ERC20, ERC20Detailed, ERC20Mintable {
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint initial_supply
-    )
-        ERC20Detailed(name, symbol, 18)
-        public
+    constructor(string memory name,string memory symbol,uint initial_supply) 
+        ERC20Detailed(name, symbol, 18) public
     {
         // constructor can stay empty
     }
--- BillyCoinCrowdsale.sol
+++ BillyCoinCrowdsale.sol
@@ -7,13 +7,20 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
-// @TODO: Inherit the crowdsale contracts
-contract PupperCoinSale is {
+contract PupperCoinSale is Crowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale, MintedCrowdsale {
 
     constructor(
-        // @TODO: Fill in the constructor parameters!
+        uint rate,
+        address payable wallet,
+        PupperCoin token,
+        uint goal,
+        uint open,
+        uint close
     )
-        // @TODO: Pass the constructor parameters to the crowdsale contracts.
+        Crowdsale(rate, wallet, token) // Pass the constructor parameters to the crowdsale contracts.
+        TimedCrowdsale(now, now + 24 weeks)
+        CappedCrowdsale(goal)
+        RefundableCrowdsale(goal)
         public
     {
         // constructor can stay empty
@@ -26,14 +33,19 @@ contract PupperCoinSaleDeployer {
     address public token_address;
 
     constructor(
-        // @TODO: Fill in the constructor parameters!
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint goal
     )
         public
     {
-        // @TODO: create the PupperCoin and keep its address handy
+        PupperCoin token = new PupperCoin(name, symbol, 0); // create the PupperCoin and keep its address handy
+        token_address = address(token);
 
-        // @TODO: create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
-
+        PupperCoinSale token_sale = new PupperCoinSale(10, wallet, token, goal, now, now + 24 weeks); // create the PupperCoinSale and tell it about the token, set the goal, and set the open and close times to now and now + 24 weeks.
+        token_sale_address = address(token_sale);
+        
         // make the PupperCoinSale contract a minter, then have the PupperCoinSaleDeployer renounce its minter role
         token.addMinter(token_sale_address);
         token.renounceMinter();
